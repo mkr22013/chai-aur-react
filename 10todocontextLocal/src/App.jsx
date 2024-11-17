33664c1ace97ebcc3a2bd42aabc98242e82ab191
mkr22013 <mkr22013@outlook.com>
2024-11-16 22:27:40 -0800

    Added new project Context API with LocalStorage

---
+++ 10todocontextLocal/src/App.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { TodoProvider } from "./context";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [...prev, { id: Date.now(), ...todo }]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id ? { ...prevTodo, todo: todo } : prevTodo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  //This useEffect is used only one time when page loads and it will get todos from local storage
+  //there is no dependency for this useEffect
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) setTodos(todos);
+  }, []);
+
+  //We also need to load todos in local storage when ever there is any new addition in todos
+  //Hence this useEffect will just add the todos to local storage whenever there is any change
+  //We have dependecy here on todos
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
+
+export default App;
